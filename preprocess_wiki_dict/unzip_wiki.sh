240109ba9ff76d73ba305066f58edfe651860a0e
Garry Kuwanto <gkuwanto@gmail.com>
2022-11-07 20:41:55 -0500

    fix typo

--- preprocess_wiki_dict/unzip_wiki.sh
+++ preprocess_wiki_dict/unzip_wiki.sh
@@ -10,10 +10,10 @@ if ! [ -f ./data_dir/wiki.${right}.xml ]; then
     bzip2 -dk ./data_dir/wiki.${right}.xml.bz2
 fi
 
-if [ ! -d ./data_dir/wiki-${left} ];
+if [ ! -d ./data_dir/wiki-${left} ]; then
     python -m wikiextractor.WikiExtractor --processes 2 -b 1G -o ./data_dir/wiki-${left} -l ./data_dir/wiki.${left}.xml
 fi
 
-if [ ! -d ./data_dir/wiki-${rigth} ];
+if [ ! -d ./data_dir/wiki-${rigth} ]; then
     python -m wikiextractor.WikiExtractor --processes 2 -b 1G -o ./data_dir/wiki-${right} -l ./data_dir/wiki.${right}.xml
 fi
