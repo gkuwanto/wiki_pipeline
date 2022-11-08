7a10fa8a369c1a45c64c40aac4d546e0e1da1857
Garry Kuwanto <gkuwanto@scc2.bu.edu>
2022-11-07 20:19:42 -0500

    chmod +x

--- preprocess_wiki_dict/pickle_wiki.sh
+++ preprocess_wiki_dict/pickle_wiki.sh
@@ -2,8 +2,8 @@ left=$1
 right=$2
 
 if ! [ -f ./data_dir/${left}/dump.pickle ]; then
-    python get_pickle.py not_load ${left} ./data_dir
+    python preprocess_wiki_dict/get_pickle.py not_load ${left} ./data_dir
 fi
 if ! [ -f ./data_dir/${right}/dump.pickle ]; then
-    python get_pickle.py not_load ${right} ./data_dir
+    python preprocess_wiki_dict/get_pickle.py not_load ${right} ./data_dir
 fi
