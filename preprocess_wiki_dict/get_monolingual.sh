917c526850d9cd22b0d3626089c96117eee3bae5
Garry Kuwanto <gkuwanto@gmail.com>
2022-11-07 20:30:29 -0500

    add monolingual bash

--- preprocess_wiki_dict/get_monolingual.sh
+++ preprocess_wiki_dict/get_monolingual.sh
@@ -1,2 +1,10 @@
 left=$1
 right=$2
+
+if ! [ -f ./data_dir/wiki.${left} ]; then
+    python preprocess_wiki_dict/get_mono.py ./data_dir/${left}-dump.pickle ./data_dir/wiki.${left}
+fi
+
+if ! [ -f ./data_dir/wiki.${right} ]; then
+    python preprocess_wiki_dict/get_mono.py ./data_dir/${right}-dump.pickle ./data_dir/wiki.${right}
+fi
