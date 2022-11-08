ba62271de92ccc74ce77cb2dd17b0f927ce30c62
Garry Kuwanto <gkuwanto@gmail.com>
2022-11-07 19:59:45 -0500

    fix logic not

--- download_wiki/download.sh
+++ download_wiki/download.sh
@@ -2,10 +2,10 @@ left=$1
 right=$2
 mkdir -p ./data_dir
 
-if [-f ./data_dir/wiki.${left}.xml.bz2]; then
+if ! [ -f ./data_dir/wiki.${left}.xml.bz2 ]; then
     wget https://dumps.wikimedia.org/${left}wiki/20221101/${left}wiki-20221101-pages-articles-multistream.xml.bz2 --no-check-certificate -O ./data_dir/wiki.${left}.xml.bz2
 fi
 
-if [-f ./data_dir/wiki.${right}.xml.bz2]; then
+if ! [ -f ./data_dir/wiki.${right}.xml.bz2 ]; then
     wget https://dumps.wikimedia.org/${right}wiki/20221101/${right}wiki-20221101-pages-articles-multistream.xml.bz2 --no-check-certificate -O ./data_dir/wiki.${right}.xml.bz2
 fi
