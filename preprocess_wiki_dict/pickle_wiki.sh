f2b2efc83abbf54a660c18eae099834225e450c7
Garry Kuwanto <gkuwanto@gmail.com>
2022-11-07 20:17:07 -0500

    fix space

--- preprocess_wiki_dict/pickle_wiki.sh
+++ preprocess_wiki_dict/pickle_wiki.sh
@@ -1,9 +1,9 @@
 left=$1
 right=$2
 
-if ![ -f ./data_dir/${left}/dump.pickle ]; then
+if ! [ -f ./data_dir/${left}/dump.pickle ]; then
     python get_pickle.py not_load ${left} ./data_dir
 fi
-if ![ -f ./data_dir/${right}/dump.pickle ]; then
+if ! [ -f ./data_dir/${right}/dump.pickle ]; then
     python get_pickle.py not_load ${right} ./data_dir
 fi
