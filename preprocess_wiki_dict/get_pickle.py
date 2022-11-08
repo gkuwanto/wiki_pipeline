ce698f132ead2a8271a6856301af5740d5c7c4d4
Garry Kuwanto <gkuwanto@scc2.bu.edu>
2022-11-07 20:23:58 -0500

    change backup folder

--- preprocess_wiki_dict/get_pickle.py
+++ preprocess_wiki_dict/get_pickle.py
@@ -21,7 +21,7 @@ def get_list_of_folder(language):
 
 def save(dictionary, checkpoint):
     print(checkpoint)
-    with open(f'{path}/{language}/backup/dump.pickle', 'wb') as handle:
+    with open(f'{path}/{language}/dump.pickle', 'wb') as handle:
         pickle.dump(dictionary, handle)
     with open('data_dir/checkpoint', 'w') as txtfile:
         print(checkpoint, file=txtfile)
@@ -30,7 +30,7 @@ def save(dictionary, checkpoint):
 def load():
     with open(f'{path}/{language}/checkpoint', 'r') as file:
         checkpoint = file.read().strip()
-    with open(f'{path}/{language}/backup/dump.pickle', 'rb') as handle:
+    with open(f'{path}/{language}/dump.pickle', 'rb') as handle:
         dictionary = pickle.load(handle)
     return dictionary, checkpoint
 
